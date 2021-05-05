1083917a8e1517094153e2ad191d75238bc19bb0
rishabh9898 <rishabh.ruhela@ucalgary.ca>
2021-05-05 19:08:01 +0400

    TweetAnalysis

---
+++ pythonFiles/TweetsAnalysis.py
@@ -0,0 +1,32 @@
+from tweepy import Stream
+from tweepy import OAuthHandler
+from tweepy.streaming import StreamListener
+import time
+import json
+
+
+#consumer key, consumer secret, access token, access secret.
+ckey=""
+csecret=""
+atoken=""
+asecret=""
+
+class listener(StreamListener):
+
+    def on_data(self, data):
+        all_data = json.loads(data)
+
+        tweet = all_data["text"]
+
+        print((tweet))
+
+        return True
+
+    def on_error(self, status):
+        print(status)
+
+auth = OAuthHandler(ckey, csecret)
+auth.set_access_token(atoken, asecret)
+
+twitterStream = Stream(auth, listener())
+twitterStream.filter(track=["car"])
